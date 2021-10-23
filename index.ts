c72acb722b396736afc607f02b76313b667da964
cham1985 <397866153@qq.com>
2021-10-23 22:23:38 +0800

    no message

--- index.ts
+++ index.ts
@@ -6,7 +6,9 @@ import * as Math from './tools/Math'
 import objTools from './tools/objTools'
 import * as stringTools from './tools/stringTools'
 import tool from './tools/tool'
+// @ts-ignore
+import rootLess from './style/root.less'
 
 export {
-  ahooks, arrayTools, dateTools, Math, objTools, stringTools, tool
+  ahooks, arrayTools, dateTools, Math, objTools, stringTools, tool, rootLess
 }
