5f6dabe23e109820848b2c3d220b644c2c479f35
stark <397866153@qq.com>
2021-10-25 16:30:05 +0800

    no message

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 
-import ahooks from './tools/ahooks'
-import arrayTools from './tools/arrayTools'
-import dateTools from './tools/dateTools'
-import * as Math from './tools/Math'
-import objTools from './tools/objTools'
-import * as stringTools from './tools/stringTools'
-import tool from './tools/tool'
+import ahooks from './src/tools/ahooks'
+import arrayTools from './src/tools/arrayTools'
+import dateTools from './src/tools/dateTools'
+import * as Math from './src/tools/Math'
+import objTools from './src/tools/objTools'
+import * as stringTools from './src/tools/stringTools'
+import tool from './src/tools/tool'
 // @ts-ignore
-import rootLess from './style/root.less'
+import rootLess from './src/style/root.less'
 
 export {
   ahooks, arrayTools, dateTools, Math, objTools, stringTools, tool, rootLess
